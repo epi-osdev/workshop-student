4044c7be06b852b4347b0198015bee5de875502e
pulk66-s <hugo.marrasse@epitech.eu>
2022-11-22 18:06:41 +0100

    [DELETE] Deleting of test purpose function

--- src/entry/kernel_entry.c
+++ src/entry/kernel_entry.c
@@ -8,8 +8,4 @@ void init()
 void kernel_main()
 {
     init();
-    vga_putchar_at('H', 0x0F, 0, 0);
-    char str[] = "Hello World!";
-    vga_putstr_at(str, 0x0F, 0, 1);
-    vga_putchar_at('H', 0x0F, 0, 2);
 }
