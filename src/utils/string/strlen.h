6547a60415cd398c300d269aacf5b7d35d40645d
Victor Boudet <victor.boudet@epitech.eu>
2022-11-25 12:57:04 +0100

    [ADD] adding strlen header

---
+++ src/utils/string/strlen.h
@@ -0,0 +1,13 @@
+#ifndef __EOS_UTILS_STRING_STRLEN_H__
+    #define __EOS_UTILS_STRING_STRLEN_H__
+
+    #include "types.h"
+
+/*
+* @brief: a function to get the lenght of a string
+* @param str: the string that will recover the size
+* @return: the value of the lenght of the string
+*/
+size_t strlen(const char *str);
+
+#endif
